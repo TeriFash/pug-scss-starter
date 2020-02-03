dda70268b1791f7a74acad6bf21b52036871aaf2
TeriFash <teri.fash@gmail.com>
2020-02-03 07:35:06 +0200

    first commit

---
+++ src/js/about.js
@@ -0,0 +1,10 @@
+import '../scss/app.scss';
+
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+UIkit.use(Icons);
+
+// Run your page code here.
+
+console.log('hello about.html');
